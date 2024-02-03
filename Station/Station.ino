6071f4888b4395eb8e963a76b4b9915ce0535a51
Samuele Capacci <samuele_capacci@outlook.it>
2024-02-03 11:03:10 +0100

    continuing

--- Station/Station.ino
+++ Station/Station.ino
@@ -1,4 +1,16 @@
+#define U_ID_SERIAL_LENGTH 16
+#define U_ID_SPECIAL_BROADCAST "0000000000000000"
+#define U_ID_SPECIAL_ANY "F00000000000000F"
+
 #define U_ID_SERIAL "0200040000000001"
+
+//COMM TO NEXT (RIGHT)
+#define TRX_RIGHT 13
+#define RCX_RIGHT 12
+
+//COMM TO PREVIOUS (LEFT)
+#define RCX_LEFT 10
+#define TRX_LEFT 9
 
 void setup() {
   // put your setup code here, to run once:
